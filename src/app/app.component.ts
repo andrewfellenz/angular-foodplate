600579c652406f9dab5e0c83776dd84afc2101ea
Andrew Fellenz <andrewfellenz@gmail.com>
2021-08-23 09:54:39 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fp-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
