490cd9e21c02092130452a3519302a388eaec3e5
Andrew Fellenz <andrewfellenz@gmail.com>
2021-08-23 14:04:55 -0500

    about to start event binding

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fp-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  version: string = "1.0.0";
+  versionNum: number = 0;
+  icon: string = "assets/images/icons/icons-29.png";
+  logoAlt: string = "Foodplate Logo";
+  needsUpdate: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.versionConvertNumber();
+  }
+
+  versionConvertNumber() {
+    let num: string = '';
+    for (let i = 0; i < this.version.length; i++){
+      num += this.version.charAt(i) !== '.' ? this.version.charAt(i) : '';
+    }
+    let versionNum: string = num;
+    console.log(versionNum);
+  }
+
+}
